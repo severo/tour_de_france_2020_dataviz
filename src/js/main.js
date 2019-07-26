9f963a54e511b7fce91017a5258ffb0110ab1aa1
Sylvain Lesage <severo@rednegra.net>
2019-07-26 11:33:58 +0200

    feat: only show the 12 first riders + use scales + y depends on rank

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,28 @@
-import * as d3 from 'd3';
+import * as d3 from "d3";
 async function load() {
   const url =
-    'https://raw.githubusercontent.com/severo/tour_de_france_2019_data/master/stages.json';
+    "https://raw.githubusercontent.com/severo/tour_de_france_2019_data/master/stages.json";
   const response = await fetch(url);
   const stages = await response.json();
 
   const general = stages.reduce(
-    (general, stage, id) => {
+    (general, stage, stageId) => {
       const newStage = stage
         .map(rider => {
-          const previousId = general[id].findIndex(
+          const previousId = general[stageId].findIndex(
             r => r.number === rider.number
           );
           return {
-            previous: general[id][previousId],
+            previous: general[stageId][previousId],
             name: rider.name,
             number: rider.number,
             team: rider.team,
             time:
-              general[id][previousId].time +
+              general[stageId][previousId].time +
               rider.time -
               rider.bonif +
               rider.penal,
+            stageId: stageId + 1
           };
         })
         .sort(
@@ -37,8 +38,9 @@ async function load() {
           number: rider.number,
           team: rider.team,
           time: rider.time,
+          stageId: rider.stageId,
           rank: id + 1,
-          gap: rider.time - bestTime,
+          gap: rider.time - bestTime
         }))
       );
       return general;
@@ -50,99 +52,140 @@ async function load() {
           name: rider.name,
           number: rider.number,
           team: rider.team,
+          stageId: 0,
           time: 0,
-          gap: 0,
+          gap: 0
         }))
-        .sort((a, b) => a.number > b.number),
+        .sort((a, b) => a.number > b.number)
     ]
   );
 
-  d3.select('#stages')
-    .selectAll('span')
-    .data(general)
+  // only show the top_n riders of the last classification
+  const top_n = 12;
+  const topRiders = general[general.length - 1]
+    .sort((a, b) => b.value - a.value)
+    .slice(0, top_n)
+    .map(rider => rider.number);
+
+  const filteredGeneral = general.map(stage => {
+    return stage
+      .filter(rider => topRiders.includes(rider.number))
+      .sort((a, b) => b.value - a.value)
+      .map((rider, topId) => {
+        rider.topRank = topId;
+        return rider;
+      });
+  });
+
+  d3.select("#stages")
+    .selectAll("span")
+    .data(filteredGeneral)
     .enter()
-    .append('span')
+    .append("span")
     .text(d => `${d[0].name} - `);
 
-  svg(general);
+  svg(filteredGeneral);
 }
 
-function getX(rider) {
-  //return rider.rank * 50;
-  return rider.gap * 4 + 50;
-}
-function getPreviousX(rider) {
-  //return rider.previous.rank * 50;
-  return rider.previous.gap * 4 + 50;
-}
-
-function showLinks(riders) {
+function showLinks(riders, x, rankY, stageY) {
   riders
-    .append('line')
-    .attr('x1', getPreviousX)
-    .attr('y1', 100)
-    .attr('x2', getX)
-    .attr('y2', 0);
+    .append("line")
+    .attr("x1", rider => x(rider.previous.gap))
+    .attr(
+      "y1",
+      rider => stageY(rider.previous.stageId) + rankY(rider.previous.topRank)
+    )
+    .attr("x2", rider => x(rider.gap))
+    .attr("y2", rider => stageY(rider.stageId) + rankY(rider.topRank));
 }
 
 function showRiders(riders) {
   riders
-    .append('circle')
-    .attr('x', 0)
-    .attr('y', 0)
-    .attr('r', 20);
+    .append("circle")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("r", 20);
 
   riders
-    .append('text')
-    .attr('x', 0)
-    .attr('y', 0)
+    .append("text")
+    .attr("x", 0)
+    .attr("y", 0)
     .text(d => d.number)
-    .attr('text-anchor', 'middle')
-    .attr('dy', '0.3em');
+    .attr("text-anchor", "middle")
+    .attr("dy", "0.3em");
 }
 
-function addLinksStage(links, general, stageId, height) {
+function addLinksStage(links, general, stageId, x, rankY, stageY) {
   links
-    .append('g')
-    .attr('id', `stage${stageId - 1}to${stageId - 1}`)
-    .attr('transform', `translate(0, ${height - 50 - stageId * 100})`)
-    .selectAll('g')
+    .append("g")
+    .attr("id", `stage${stageId - 1}to${stageId - 1}`)
+    //.attr("transform", `translate(0, ${stageY(stageId)})`)
+    .selectAll("g")
     .data(general[stageId])
     .enter()
-    .append('g')
-    .attr('id', d => `stage${stageId - 1}to${stageId - 1}-rider${d.number}`)
-    .classed('link', true)
-    .call(showLinks);
+    .append("g")
+    .attr("id", d => `stage${stageId - 1}to${stageId - 1}-rider${d.number}`)
+    .classed("link", true)
+    .call(riders => showLinks(riders, x, rankY, stageY));
 }
 
-function addRidersStage(riders, general, stageId, height) {
+function addRidersStage(riders, general, stageId, x, rankY, stageY) {
   riders
-    .append('g')
-    .attr('id', `stage${stageId}`)
-    .attr('transform', `translate(0, ${height - 50 - stageId * 100})`)
-    .selectAll('g')
+    .append("g")
+    .attr("id", `stage${stageId}`)
+    .attr("transform", `translate(0, ${stageY(stageId)})`)
+    .selectAll("g")
     .data(general[stageId])
     .enter()
-    .append('g')
-    .attr('id', d => `stage${stageId}-rider${d.number}`)
-    .classed('rider', true)
-    .attr('transform', d => `translate(${getX(d)}, 0)`)
+    .append("g")
+    .attr("id", d => `stage${stageId}-rider${d.number}`)
+    .classed("rider", true)
+    .attr("transform", d => `translate(${x(d.gap)}, ${rankY(d.topRank)})`)
     .call(showRiders);
 }
 
 function svg(general) {
-  const height = 1500;
+  const margin = {
+    left: 40,
+    right: 40,
+    stages: { top: 40, bottom: 40 },
+    ranks: { top: 20, bottom: 80 }
+  };
+
+  const nbRiders = general[0].length;
+  const nbStages = general.length;
+  const rankHeight = 20;
+  const width = 800;
+  const stageHeight =
+    nbRiders * rankHeight + margin.ranks.top + margin.ranks.bottom;
+  const height = nbStages * stageHeight;
+  const maxGap = d3.max(general, stage => d3.max(stage, rider => rider.gap));
+
   const el = d3
-    .select('svg#stages-svg')
-    .attr('width', '200%')
-    .attr('height', height);
+    .select("svg#stages-svg")
+    .attr("width", width)
+    .attr("height", height);
 
-  const links = el.append('g').attr('id', 'links');
-  const riders = el.append('g').attr('id', 'riders');
+  const x = d3
+    .scaleLinear()
+    .domain([0, maxGap])
+    .range([margin.left, width - margin.right]);
+  const stageY = d3
+    .scaleLinear()
+    .domain([nbStages - 1, 0])
+    .range([margin.stages.top, height - margin.stages.bottom - stageHeight]);
+  const rankY = d3
+    .scaleLinear()
+    .domain([0, nbRiders - 1])
+    .range([margin.ranks.top, stageHeight - margin.ranks.bottom]);
 
-  general.slice(0, 15).forEach((_, stageId) => {
-    if (stageId > 0) addLinksStage(links, general, stageId, height);
-    addRidersStage(riders, general, stageId, height);
+  //debugger;
+  const links = el.append("g").attr("id", "links");
+  const riders = el.append("g").attr("id", "riders");
+
+  general.forEach((_, stageId) => {
+    if (stageId > 0) addLinksStage(links, general, stageId, x, rankY, stageY);
+    addRidersStage(riders, general, stageId, x, rankY, stageY);
   });
 }
 
