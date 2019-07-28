a4d40130a41b35ef55bf8608c451804ba13a213c
Sylvain Lesage <severo@rednegra.net>
2019-07-28 14:24:26 +0200

    refactor: modularize code

---
+++ src/js/annotations.js
@@ -0,0 +1,130 @@
+import {
+  annotationCalloutCircle,
+  annotationCalloutRect,
+  annotationCalloutElbow
+} from "d3-svg-annotation";
+
+export const stagesAnnotations = {
+  1: {
+    riderName: "JULIAN ALAPHILIPPE",
+    title: "Les favoris groupés",
+    text:
+      "Les 6 favoris sont à 10 secondes du maillot jaune Mike Teunissen pour cause de bonifications.",
+    type: annotationCalloutRect,
+    subject: { width: 30, height: 6 * 20 + 30, xOffset: -15, yOffset: -15 }
+  },
+  2: {
+    riderName: "STEVEN KRUIJSWIJK",
+    title: "Kruijswijk gagne du temps",
+    text: "Jumbo-Visma remporte le contre-la-montre par équipe, devant Ineos",
+    type: annotationCalloutCircle
+  },
+  3: {
+    riderName: "JULIAN ALAPHILIPPE",
+    title: "Alaphilippe: étape et maillot jaune",
+    text: 'Alaphilippe gagne en solitaire et prend 26" sur ses rivaux.',
+    type: annotationCalloutCircle
+  },
+  4: {
+    riderName: "JULIAN ALAPHILIPPE",
+    title: "Aucun changement",
+    text: "Viviani gagne le sprint, tous les favoris dans le peloton.",
+    type: annotationCalloutElbow
+  },
+  5: {
+    riderName: "JULIAN ALAPHILIPPE",
+    title: "Aucun changement",
+    text: "Cette fois, c'est Sagan qui remporte le sprint.",
+    type: annotationCalloutElbow
+  },
+  6: {
+    riderName: "STEVEN KRUIJSWIJK",
+    title: "Kruijswijk distancé",
+    text: `Ciccone profite de l'échapée pour ravir le maillot jaune à Alaphilippe. Dans la côte finale, Thomas reprend 2" sur Alaphilippe et Pinot, 7" sur Buchmann, 9" sur Bernal et 35" sur Kruijswijk.`,
+    type: annotationCalloutCircle
+  },
+  7: {
+    riderName: "JULIAN ALAPHILIPPE",
+    title: "Aucun changement",
+    text: "Nouvelle arrivée au sprint, réglé par Groenewegen.",
+    type: annotationCalloutElbow
+  },
+  8: {
+    riderName: "JULIAN ALAPHILIPPE",
+    title: "Alaphilippe maillot jaune",
+    text: `Alaphilippe et Pinot s'échappent dans la Côte de la Jaillière, et arrivent avec 20" (plus bonifications) sur leurs adversaires.`,
+    type: annotationCalloutCircle
+  },
+  9: {
+    riderName: "JULIAN ALAPHILIPPE",
+    title: "Aucun changement",
+    text: `Victoire d'Impey, tous les favoris arrivent ensemble, à 16'25".`,
+    type: annotationCalloutElbow
+  },
+  10: {
+    riderName: "THIBAUT PINOT",
+    title: "Pinot dans une bordure",
+    text: `Dans une étape venteuse, Pinot a été piégé par une bordure, et perd 1'40" sur ses adversaires directs.`,
+    type: annotationCalloutCircle
+  },
+  11: {
+    riderName: "JULIAN ALAPHILIPPE",
+    title: "Aucun changement",
+    text: `Ewan gagne le sprint, pas d'écart entre les favoris.`,
+    type: annotationCalloutElbow
+  },
+  12: {
+    riderName: "JULIAN ALAPHILIPPE",
+    title: "Aucun changement",
+    text: `Yates gagne à Bagnères-de-Bigorre. Les 6 favoris arrivent ensemble à 9'35".`,
+    type: annotationCalloutElbow
+  },
+  13: {
+    riderName: "JULIAN ALAPHILIPPE",
+    title: "Alaphilippe gagne le contre-la-montre",
+    text: `Alaphilippe conforte son maillot jaune, Thomas est second à 14", Kruijswijk perd 45", Pinot 49" et Buchmann 1'19". Bernal est le plus loin, à 1'36".`,
+    type: annotationCalloutCircle
+  },
+  14: {
+    riderName: "THIBAUT PINOT",
+    title: "Thibaut vainqueur au Tourmalet",
+    text: `Victoire de Thibault, Alaphilippe arrive avec Kruijswijk à 6", et les trois empochent des bonifications. Buchmann et Bernal arrivent à 8", et Thomas perd 36".`,
+    type: annotationCalloutCircle
+  },
+  15: {
+    riderName: "THIBAUT PINOT",
+    title: "Thibaut reprend encore du temps",
+    text: `Deuxième derrière Yates, Thibaut fait une bonne opération : 18" d'avance sur Buchmann et Bernal, 49" sur Thomas et Kruijswijk, et 1'49" sur Alaphilippe qui garde le maillot jaune.`,
+    type: annotationCalloutCircle
+  },
+  16: {
+    riderName: "JULIAN ALAPHILIPPE",
+    title: "Aucun changement",
+    text: `Victoire au sprint pour Ewan. Pas de changement entre les favoris.`,
+    type: annotationCalloutElbow
+  },
+  17: {
+    riderName: "JULIAN ALAPHILIPPE",
+    title: "Aucun changement",
+    text: `Les favoris arrivent ensemble, 20'10" derrière Trentin qui gagne l'étape.`,
+    type: annotationCalloutElbow
+  },
+  18: {
+    riderName: "EGAN BERNAL",
+    title: `Bernal deuxième du classement`,
+    text: `Parti dans le Galibier, Bernal prend 32" sur les autres favoris qui arrivent ensemble, Alaphilippe ayant réussi à rentrer dans la descente.`,
+    type: annotationCalloutCircle
+  },
+  19: {
+    riderName: "EGAN BERNAL",
+    title: `Bernal prend le maillot jaune`,
+    text: `Dans une étape écourtée par la météo, les temps ont été pris en haut de l'Isoard. Bernal qui avait attaqué à 6km du col, passe avec 1'06" d'avance sur Thomas, Kruijswijk et Buchmann. Alaphilippe perd 2'15" et son maillot jaune. Pinot avait abandonné sur blessure.`,
+    type: annotationCalloutCircle
+  },
+  20: {
+    riderName: "JULIAN ALAPHILIPPE",
+    title: "Alaphilippe perd pied",
+    text: `Alaphilippe passe de la deuxième à la cinquième place lors d'une étape écourtée, en perdant 3" dans la montée de 33 kilomètres. Bernal et Thomas arrivent ensemble, à 17" du vainqueur Nibali, et 6" devant Buchmann et Kruijswijk. Bernal est maillot jaune.`,
+    type: annotationCalloutCircle
+  }
+};
---
+++ src/js/load.js
@@ -0,0 +1,177 @@
+import * as d3 from "d3";
+
+// adapted from https://24ways.org/2010/calculating-color-contrast/
+function getContrastYIQ(hexcolor) {
+  const color = hexcolor[0] === "#" ? hexcolor.slice(1) : hexcolor;
+  var r = parseInt(color.substr(0, 2), 16);
+  var g = parseInt(color.substr(2, 2), 16);
+  var b = parseInt(color.substr(4, 2), 16);
+  var yiq = (r * 299 + g * 587 + b * 114) / 1000;
+  return yiq >= 128 ? "black" : "white";
+}
+
+async function getGeneralFromStages(teams) {
+  const url =
+    "https://raw.githubusercontent.com/severo/tour_de_france_2019_data/master/stages.json";
+  const response = await fetch(url);
+  const stages = await response.json();
+
+  const general = stages.reduce(
+    (general, stage, stageId) => {
+      const newStage = stage
+        .map(rider => {
+          const previousId = general[stageId].findIndex(
+            r => r.number === rider.number
+          );
+          return {
+            previous: general[stageId][previousId],
+            name: rider.name,
+            number: rider.number,
+            team: rider.team,
+            time:
+              general[stageId][previousId].time +
+              rider.time -
+              rider.bonif +
+              rider.penal,
+            stageId: stageId + 1
+          };
+        })
+        .sort(
+          (a, b) =>
+            a.time > b.time || (a.time === b.time && a.number > b.number)
+        );
+      const bestTime = newStage[0].time;
+
+      general.push(
+        newStage.map((rider, id) => ({
+          previous: rider.previous,
+          name: rider.name,
+          number: rider.number,
+          team: rider.team,
+          time: rider.time,
+          stageId: rider.stageId,
+          rank: id + 1,
+          topRank: id + 1,
+          gap: rider.time - bestTime,
+          color: teams.get(rider.team).colour,
+          labelColor: teams.get(rider.team).labelColor
+        }))
+      );
+      return general;
+    },
+    [
+      stages[0]
+        .map(rider => ({
+          rank: 1,
+          topRank: rider.number,
+          name: rider.name,
+          number: rider.number,
+          team: rider.team,
+          stageId: 0,
+          time: 0,
+          gap: 0,
+          color: teams.get(rider.team).colour,
+          labelColor: teams.get(rider.team).labelColor
+        }))
+        .sort((a, b) => a.number > b.number)
+    ]
+  );
+
+  return general;
+}
+
+async function getGeneralFromGeneralClassification(teams) {
+  const url =
+    "https://raw.githubusercontent.com/severo/tour_de_france_2019_data/master/general_classification.json";
+  const response = await fetch(url);
+  const stages = await response.json();
+
+  const general = stages.reduce(
+    (general, stage, stageId) => {
+      const bestTime = stage[0].time;
+      general.push(
+        stage.map((rider, id) => {
+          const previousId = general[stageId].findIndex(
+            r => r.number === rider.number
+          );
+          return {
+            previous: general[stageId][previousId],
+            name: rider.name,
+            number: rider.number,
+            team: rider.team,
+            time: rider.time,
+            stageId: stageId + 1,
+            rank: id + 1,
+            topRank: id + 1,
+            gap: rider.time - bestTime,
+            color: teams.get(rider.team).colour,
+            labelColor: teams.get(rider.team).labelColor
+          };
+        })
+      );
+      return general;
+    },
+    [
+      stages[0]
+        .map(rider => ({
+          rank: 1,
+          topRank: rider.number,
+          name: rider.name,
+          number: rider.number,
+          team: rider.team,
+          stageId: 0,
+          time: 0,
+          gap: 0,
+          color: teams.get(rider.team).colour,
+          labelColor: teams.get(rider.team).labelColor
+        }))
+        .sort((a, b) => a.number > b.number)
+    ]
+  );
+
+  return general;
+}
+
+export async function load(
+  { filtered, from } = { filtered: true, from: "general" }
+) {
+  const teams = await d3
+    .json(
+      "https://raw.githubusercontent.com/severo/tour_de_france_2019_data/master/teams.json"
+    )
+    .then(
+      json =>
+        new Map(
+          json.map(team => {
+            team.labelColor = getContrastYIQ(team.colour);
+            return [team.name, team];
+          })
+        )
+    );
+
+  const general =
+    from === "stages"
+      ? await getGeneralFromStages(teams)
+      : await getGeneralFromGeneralClassification(teams);
+
+  // only show the top_n riders of the last classification
+  const top_n = 6;
+  const stageId = 17;
+  //const stageId = general.length - 1;
+  const topRiders = general[stageId]
+    .sort((a, b) => b.value - a.value)
+    .slice(0, top_n)
+    .map(rider => rider.number);
+
+  const filteredGeneral = general.map(stage => {
+    return stage
+      .filter(rider => topRiders.includes(rider.number))
+      .sort((a, b) => b.value - a.value)
+      .map((rider, topId) => {
+        rider.topRank = topId;
+        return rider;
+      });
+  });
+
+  return filtered ? filteredGeneral : general;
+}
